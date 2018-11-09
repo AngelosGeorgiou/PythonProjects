3a5f08f498adfcdad612a4e269974fa466aa81ab
AngelosGeorgiou <angelosgeorgiou000@gmail.com>
2018-11-09 12:33:27 +0200

    new kata

---
+++ UnknownDigit.py
@@ -0,0 +1,67 @@
+# o give credit where credit is due: This problem was taken from the ACMICPC-
+# Northwest Regional Programming Contest. Thank you problem writers.
+
+# You are helping an archaeologist decipher some runes. He knows that this
+# ancient society used a Base 10 system, and that they never start a number
+# with a leading zero. He's figured out most of the digits as well as a few
+# operators, but he needs your help to figure out the rest.
+
+# The professor will give you a simple math expression, of the form
+
+# [number][op][number]=[number]
+
+# He has converted all of the runes he knows into digits. The only operators
+# he knows are addition (+),subtraction(-), and multiplication (*), so those
+# are the only ones that will appear. Each number will be in the range from
+# -1000000 to 1000000, and will consist of only the digits 0-9, possibly a
+# leading -, and maybe a few ?s. If there are ?s in an expression, they
+# represent a digit rune that the professor doesn't know (never an operator,
+# and never a leading -). All of the ?s in an expression will represent the
+# same digit (0-9), and it won't be one of the other given digits in the
+# expression. No number will begin with a 0 unless the number itself is 0,
+# therefore 00 would not be a valid number.
+
+# Given an expression, figure out the value of the rune represented by the
+# question mark. If more than one digit works, give the lowest one. If no
+# digit works, well, that's bad news for the professor - it means that he's
+# got some of his runes wrong. output -1 in that case.
+
+# Complete the method to solve the expression to find the value of the unknown
+# rune. The method takes a string as a paramater repressenting the expression
+# and will return an int value representing the unknown rune or -1 if no such
+# rune exists.
+
+
+def solve_runes(runes):
+	s = '+' if '+' in runes else '*' if '*' in runes else 'k'
+	if s == 'k':
+		i = runes[1:].find('-')
+		runes = runes[:i+1] + 'k' + runes[i+2:] if len(runes)>i else ""
+
+	operands = runes.split('=')[0].split(s) + [runes.split('=')[1]]
+	print(operands)
+	for quest in range(0,10):
+		if quest == 0 and any(operand.startswith('?') and len(operand)>1 for operand in operands) or (str(quest) in runes): continue
+		if s == '+':
+			if int(operands[0].replace('?',str(quest))) + int(operands[1].replace('?',str(quest))) == int(operands[2].replace('?',str(quest))): return quest 
+		elif s == '*':
+			if int(operands[0].replace('?',str(quest))) * int(operands[1].replace('?',str(quest))) == int(operands[2].replace('?',str(quest))): return quest 
+		else:
+			if int(operands[0].replace('?',str(quest))) - int(operands[1].replace('?',str(quest))) == int(operands[2].replace('?',str(quest))): return quest 
+	return -1
+
+print(solve_runes("1+1=?")) #, 2, "Answer for expression '1+1=?' ");
+print(solve_runes("123*45?=5?088")) #, 6, "Answer for expression '123*45?=5?088' ");
+print(solve_runes("-5?*-1=5?")) #, 0, "Answer for expression '-5?*-1=5?' ");
+print(solve_runes("19--45=5?")) #, -1, "Answer for expression '19--45=5?' ");
+print(solve_runes("??*??=302?")) #, 5, "Answer for expression '??*??=302?' ");
+print(solve_runes("?*11=??")) #, 2, "Answer for expression '?*11=??' ");
+print(solve_runes("??*1=??")) #, 2, "Answer for expression '?*11=??' ");
+
+#a better solution https://www.codewars.com/kata/reviews/59b944f76ec86ee3ec001871/groups/59d23d376bbc7f5631001525
+
+def solve_runes(runes):
+    for c in sorted(set('0123456789') - set(runes)):        
+        s = runes.replace('?', c).replace('-', ' - ').replace('+', ' + ').replace('*', ' * ').replace('=', ' == ')
+        if not any(e[0] == '0' and e != '0' for e in s.split()) and eval(s): return int(c)
+    return -1
